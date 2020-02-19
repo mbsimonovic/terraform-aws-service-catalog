2bdac559b112ab92af0c0c0394acf6a054b1cc95
Yevgeniy Brikman <brikis98@gmail.com>
2020-02-19 16:39:13 +0100

    Add required_providers block

--- modules/networking/mock-vpc/main.tf
+++ modules/networking/mock-vpc/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_providers {
+    # Provider version 2.X series is the latest, but has breaking changes with 1.X series.
+    aws = "~> 2.6"
+  }
+}
+
 resource "random_pet" "example" {
   keepers = {
     vpc_name = var.vpc_name
