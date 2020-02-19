1070380a1be383997fba97f851c8de906deeb9b9
Marina Limeira <marinaflessa@gmail.com>
2020-02-19 14:14:51 +0100

    Init mock_vpc tests

---
+++ test/mock_vpc_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMockVpc(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../modules/mock-vpc",
+		Vars: map[string]interface{}{
+			"vpc_name": "oie",
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	vpc_id := terraform.Output(t, terraformOptions, "vpc_id")
+	assert.Regexp(t, "^vpc-.*", vpc_id)
+}
