ea11eb0fa4b28112084c01796c08afffdfb7423e
Zack Proser <zackproser@gmail.com>
2020-03-02 15:39:34 -0800

    Pin alb module to v0.18.0

--- examples/for-learning-and-testing/networking/alb/main.tf
+++ examples/for-learning-and-testing/networking/alb/main.tf
@@ -10,7 +10,7 @@ module "alb" {
   # When using these modules in your own repos, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/networking/alb?ref=v1.0.8"
-  source = "../../../../modules/networking/alb"
+  source = "../../../../modules/networking/alb?ref=v0.18.0"
 
   alb_name = var.alb_name
 
