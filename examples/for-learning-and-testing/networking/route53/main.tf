91af883b2e7013c5ba443a588e7d6afcaeff9865
Zack Proser <zackproser@gmail.com>
2020-03-09 13:45:09 -0700

    Rename route53-private -> route53

--- examples/for-learning-and-testing/networking/route53/main.tf
+++ examples/for-learning-and-testing/networking/route53/main.tf
@@ -9,8 +9,8 @@ provider "aws" {
 module "route53-private" {
   # When using these modules in your own repos, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/networking/route53-private?ref=v1.2.3"
-  source = "../../../../modules/networking/route53-private"
+  # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/networking/route53?ref=v1.2.3"
+  source = "../../../../modules/networking/route53"
 
   internal_services_domain_name = var.internal_services_domain_name
 
