4bcb78d13f9368bc674f2a17852986354d62023f
Zack Proser <zackproser@gmail.com>
2020-03-09 13:35:53 -0700

    Fix example URL in comment

--- examples/for-learning-and-testing/networking/route53-private/main.tf
+++ examples/for-learning-and-testing/networking/route53-private/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 module "route53-private" {
   # When using these modules in your own repos, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/mgmt/jenkins?ref=v1.0.8"
+  # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/networking/route53-private?ref=v1.2.3"
   source = "../../../../modules/networking/route53-private"
 
   internal_services_domain_name = var.internal_services_domain_name
