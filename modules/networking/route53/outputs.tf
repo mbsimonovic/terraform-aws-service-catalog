8cf66b11d155d4b29ba5911ec9d96ca7052d2c99
Zack Proser <zackproser@gmail.com>
2020-03-12 13:26:03 -0700

    Make output definitions more legible

--- modules/networking/route53/outputs.tf
+++ modules/networking/route53/outputs.tf
@@ -1,23 +1,23 @@
 output "private_domain_names" {
-  value = [for z in aws_route53_zone.private_zones : z.name]
+  value = values(aws_route53_zone.private_zones)[*].name
 }
 
 output "private_zones_ids" {
-  value = [for z in aws_route53_zone.private_zones : z.zone_id]
+  value = values(aws_route53_zone.private_zones)[*].zone_id 
 }
 
 output "private_zones_name_servers" {
-  value = [for z in aws_route53_zone.private_zones : z.name_servers]
+  value = values(aws_route53_zone.private_zones)[*].name_servers
 }
 
 output "public_domain_names" {
-  value = [for z in aws_route53_zone.public_zones : z.name]
+  value = values(aws_route53_zone.public_zones)[*].name
 }
 
 output "public_hosted_zones_ids" {
-  value = [for z in aws_route53_zone.public_zones : z.id]
+  value = values(aws_route53_zone.public_zones)[*].id
 }
 
 output "public_hosted_zones_name_servers" {
-  value = [for z in aws_route53_zone.public_zones : z.name_servers]
+  value = values(aws_route53_zone.public_zones)[*].name_servers
 }
