816029ea78834354486102a64aec6a1d2f68f0bf
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-03-12 17:00:39 -0700

    Back out all the mysql testing code

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -1,15 +1,11 @@
 package test
 
 import (
-	"database/sql"
-	"fmt"
 	"os"
 	"testing"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/require"
 )
 
 // Test constants for the Gruntwork Phx DevOps account
@@ -33,34 +29,6 @@ var acmRegionsForTest = []string{
 
 // Tags in Gruntwork Phx DevOps account to uniquely find Hosted Zone for baseDomainForTest
 var domainNameTagsForTest = map[string]interface{}{"original": "true"}
-
-type RDSInfo struct {
-	Username   string
-	Password   string
-	DBName     string
-	DBEndpoint string
-	DBPort     string
-}
-
-func smokeTestMysql(t *testing.T, serverInfo RDSInfo) {
-	dbConnString := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s",
-		serverInfo.Username,
-		serverInfo.Password,
-		serverInfo.DBEndpoint,
-		serverInfo.DBPort,
-		serverInfo.DBName,
-	)
-	db, connErr := sql.Open("mysql", dbConnString)
-	require.NoError(t, connErr)
-	defer db.Close()
-
-	row := db.QueryRow("SELECT 1+1;")
-	var result string
-	scanErr := row.Scan(&result)
-	require.NoError(t, scanErr)
-	require.Equal(t, result, "2")
-}
 
 // Some of the tests need to run against Organization root account. This method overrides the default AWS_* environment variables
 func configureTerraformForOrgTestAccount(t *testing.T, terraformOptions *terraform.Options) {
