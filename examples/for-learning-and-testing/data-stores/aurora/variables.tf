42277f3fea5b979edbaca067013148692bbac44b
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-03-13 02:28:20 -0700

    Set reasonable defaults in the testing example

--- examples/for-learning-and-testing/data-stores/aurora/variables.tf
+++ examples/for-learning-and-testing/data-stores/aurora/variables.tf
@@ -4,6 +4,7 @@
 # module.
 # Set using the env var TF_VAR_varname.
 # ---------------------------------------------------------------------------------------------------------------------
+
 # TF_VAR_master_password
 variable "master_password" {
   description = "The password for the master user."
@@ -11,32 +12,32 @@ variable "master_password" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
+# OPTIONAL PARAMETERS
 # ---------------------------------------------------------------------------------------------------------------------
 
 variable "name" {
   description = "The name used to namespace all the Aurora resources created by these templates, including the cluster and cluster instances (e.g. drupaldb). Must be unique in this region. Must be a lowercase string."
   type        = string
+  default     = "aurora"
 }
 
 variable "master_username" {
   description = "The username for the master user."
   type        = string
+  default     = "aurora"
 }
 
 variable "db_name" {
   description = "The name for your database of up to 8 alpha-numeric characters. If you do not provide a name, Amazon RDS will not create a database in the DB cluster you are creating."
   type        = string
+  default     = "aurora"
 }
 
 variable "share_snapshot_with_account_id" {
   description = "The ID of the AWS Account that the snapshot should be shared with."
   type        = string
+  default     = null
 }
-
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# ---------------------------------------------------------------------------------------------------------------------
 
 variable "engine" {
   description = "The name of the database engine to be used for the RDS instance. Must be one of: aurora, aurora-postgresql."
