7a7c2392506b299f619fd3260c2c1dd1c544d1e4
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-03-13 13:45:31 -0700

    Add plan testing for complicated logic with policies

--- test/ecr_repos_test.go
+++ test/ecr_repos_test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
@@ -12,6 +15,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestECRRepositories(t *testing.T) {
@@ -121,4 +125,155 @@ func TestECRRepositories(t *testing.T) {
 		out := shell.RunCommandAndGetOutput(t, testCmd)
 		assert.Contains(t, out, "Hello from Docker!")
 	})
+}
+
+// TODO: Once terratest has support for testing with plan files, update with plan testing for the various merge
+// functionalities. In lieu of that, we do the more brittle count based tests here for now.
+
+type IAMPoliciesTestCase struct {
+	name                          string
+	shouldCreate                  bool
+	defaultReadAccessAccountIDs   []string
+	defaultWriteAccessAccountIDs  []string
+	overrideWriteAccessAccountIDs []string
+	overrideReadAccessAccountIDs  []string
+}
+
+func TestECRRepositoryIAMPoliciesLogic(t *testing.T) {
+	t.Parallel()
+
+	testCases := []IAMPoliciesTestCase{
+		IAMPoliciesTestCase{
+			name:                          "test-all-empty",
+			shouldCreate:                  false,
+			overrideReadAccessAccountIDs:  nil,
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: nil,
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-override-read-access",
+			shouldCreate:                  true,
+			overrideReadAccessAccountIDs:  []string{"11111111"},
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: nil,
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-override-read-access-empty",
+			shouldCreate:                  false,
+			overrideReadAccessAccountIDs:  []string{},
+			defaultReadAccessAccountIDs:   []string{"111111111"},
+			overrideWriteAccessAccountIDs: nil,
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-default-read-access",
+			shouldCreate:                  true,
+			overrideReadAccessAccountIDs:  nil,
+			defaultReadAccessAccountIDs:   []string{"11111111"},
+			overrideWriteAccessAccountIDs: nil,
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-override-write-access",
+			shouldCreate:                  true,
+			overrideReadAccessAccountIDs:  nil,
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: []string{"11111111"},
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-override-write-access-empty",
+			shouldCreate:                  false,
+			overrideReadAccessAccountIDs:  nil,
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: []string{},
+			defaultWriteAccessAccountIDs:  []string{"111111111"},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-default-write-access",
+			shouldCreate:                  true,
+			overrideReadAccessAccountIDs:  nil,
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: nil,
+			defaultWriteAccessAccountIDs:  []string{"11111111"},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-override-both",
+			shouldCreate:                  true,
+			overrideReadAccessAccountIDs:  []string{"11111111"},
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: []string{"11111111"},
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+		IAMPoliciesTestCase{
+			name:                          "test-default-both",
+			shouldCreate:                  true,
+			overrideReadAccessAccountIDs:  []string{"11111111"},
+			defaultReadAccessAccountIDs:   []string{},
+			overrideWriteAccessAccountIDs: []string{"11111111"},
+			defaultWriteAccessAccountIDs:  []string{},
+		},
+	}
+
+	for _, testCase := range testCases {
+		// We capture the range variable to bring into the scope of the for loop, to avoid it changing while the
+		// subtests are running.
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			testFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/for-learning-and-testing/data-stores/ecr-repos")
+			awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+			uniqueID := strings.ToLower(random.UniqueId())
+
+			name := fmt.Sprintf("sample-app-%s", strings.ToLower(uniqueID))
+			tfvars := map[string]interface{}{
+				"aws_region": awsRegion,
+				"repositories": map[string]interface{}{
+					name: map[string]interface{}{
+						"external_account_ids_with_read_access":  testCase.overrideReadAccessAccountIDs,
+						"external_account_ids_with_write_access": testCase.overrideWriteAccessAccountIDs,
+						"tags":                                   map[string]string{},
+						"enable_automatic_image_scanning":        true,
+					},
+				},
+				"default_external_account_ids_with_read_access":  testCase.defaultReadAccessAccountIDs,
+				"default_external_account_ids_with_write_access": testCase.defaultWriteAccessAccountIDs,
+			}
+			// We work around a terraform bug where we can't pass in null values to terraform on the CLI by using tfvars
+			// files
+			options, varFilesFname := constructTerraformOptionsWithVarFiles(t, testFolder, tfvars)
+			defer os.Remove(varFilesFname)
+			planOut := terraform.InitAndPlan(t, options)
+			resourceCounts := terraform.GetResourceCount(t, planOut)
+			if testCase.shouldCreate {
+				assert.Equal(t, resourceCounts.Add, 2)
+			} else {
+				assert.Equal(t, resourceCounts.Add, 1)
+			}
+			assert.Equal(t, resourceCounts.Change, 0)
+			assert.Equal(t, resourceCounts.Destroy, 0)
+		})
+	}
+}
+
+func constructTerraformOptionsWithVarFiles(t *testing.T, terraformDir string, vars map[string]interface{}) (*terraform.Options, string) {
+	out, err := json.Marshal(vars)
+	require.NoError(t, err)
+	fname := func() string {
+		f, err := ioutil.TempFile("", "*.tfvars.json")
+		require.NoError(t, err)
+		defer f.Close()
+		_, writeErr := f.Write(out)
+		require.NoError(t, writeErr)
+		return f.Name()
+	}()
+	terraformOptions := &terraform.Options{
+		TerraformDir: terraformDir,
+		VarFiles:     []string{fname},
+	}
+	return terraformOptions, fname
 }
