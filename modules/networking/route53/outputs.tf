1e7893d9f4d2d799b64988b1695c1cc5fa48bc86
Zack Proser <zackproser@gmail.com>
2020-03-17 11:09:01 -0700

    Add descriptions to module outputs

--- modules/networking/route53/outputs.tf
+++ modules/networking/route53/outputs.tf
@@ -1,23 +1,29 @@
 output "private_domain_names" {
-  value = values(aws_route53_zone.private_zones)[*].name
+  description = "The names of the internal-only Route 53 Hosted Zones"
+  value       = values(aws_route53_zone.private_zones)[*].name
 }
 
 output "private_zones_ids" {
-  value = values(aws_route53_zone.private_zones)[*].zone_id 
+  description = "The IDs of the internal-only Route 53 Hosted Zones"
+  value       = values(aws_route53_zone.private_zones)[*].zone_id
 }
 
 output "private_zones_name_servers" {
-  value = values(aws_route53_zone.private_zones)[*].name_servers
+  description = "The name servers associated with the internal-only Route 53 Hosted Zones"
+  value       = values(aws_route53_zone.private_zones)[*].name_servers
 }
 
 output "public_domain_names" {
-  value = values(aws_route53_zone.public_zones)[*].name
+  description = "The names of the public Route 53 Hosted Zones"
+  value       = values(aws_route53_zone.public_zones)[*].name
 }
 
 output "public_hosted_zones_ids" {
-  value = values(aws_route53_zone.public_zones)[*].id
+  description = "The IDs of the public Route 53 Hosted Zones"
+  value       = values(aws_route53_zone.public_zones)[*].id
 }
 
 output "public_hosted_zones_name_servers" {
-  value = values(aws_route53_zone.public_zones)[*].name_servers
+  description = "The name servers associated with the public Route 53 Hosted Zones"
+  value       = values(aws_route53_zone.public_zones)[*].name_servers
 }
