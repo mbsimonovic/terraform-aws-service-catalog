de7f42b134d25b1c1ecd87931a768c34b88c1936
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-04-02 20:33:11 -0700

    [skip ci] Apply suggestions from code review
    Co-Authored-By: Ben Whaley <bwhaley@gmail.com>

--- examples/for-learning-and-testing/services/k8s-namespace/outputs.tf
+++ examples/for-learning-and-testing/services/k8s-namespace/outputs.tf
@@ -1,14 +1,14 @@
 output "namespace_name" {
-  description = "the name of the created namespace."
+  description = "The name of the created namespace."
   value       = module.namespace.namespace_name
 }
 
 output "namespace_rbac_access_all_role" {
-  description = "the name of the rbac role that grants admin level permissions on the namespace."
+  description = "The name of the rbac role that grants admin level permissions on the namespace."
   value       = module.namespace.namespace_rbac_access_all_role
 }
 
 output "namespace_rbac_access_read_only_role" {
-  description = "the name of the rbac role that grants read only permissions on the namespace."
+  description = "The name of the rbac role that grants read only permissions on the namespace."
   value       = module.namespace.namespace_rbac_access_read_only_role
 }
