a3ca0cf225c6e849166fa6a932aee51016258add
Marina Limeira <marinaflessa@gmail.com>
2020-04-08 20:57:20 +0200

    Remove acm_tls_certificates from locals
    - This fix dependency issue from route53_zone

--- modules/networking/route53/main.tf
+++ modules/networking/route53/main.tf
@@ -66,37 +66,26 @@ module "acm-tls-certificates" {
   # TODO: Replace me when the latest module-load-balancer ref is released 
   source = "../../../../module-load-balancer/modules/acm-tls-certificate/"
 
-  # Pass in the nested map of certificate requests 
+  # Pass in the nested map of certificate requests
   # built locally from var.public_zones 
-  acm_tls_certificates = local.acm_tls_certificates
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# LOCAL VARIABLES
-# ---------------------------------------------------------------------------------------------------------------------
-
-locals {
-  # Create nested map of ACM certificates to request 
-  # in the formmat expected by the ACM module 
   acm_tls_certificates = {
-    for domain, zone in var.public_zones :
-    # These certificates that are going to be 
-    # automatically issued and verified for 
-    # public Route53 zones, so we prefix them 
-    # with *. to denote we are requesting 
-    # "wildcard" certificates 
+    for domain, zone in aws_route53_zone.public_zones:
+    # These certificates that are going to be
+    # automatically issued and verified for
+    # public Route53 zones, so we prefix them
+    # with *. to denote we are requesting
+    # "wildcard" certificates
 
-    # A wildcard certificate for example.com,  
-    # requested with a domain of *.example.com, 
+    # A wildcard certificate for example.com,
+    # requested with a domain of *.example.com,
     # will protect all one-level subdomains of example.com,
     # such as mail.example.com, admin.example.com, etc
-    "*.${domain}" => {
+    "*.${zone.name}" => {
       tags                       = zone.tags
       create_verification_record = true
       verify_certificate         = true
-      bare_domain                = domain
-      vpc_id                     = zone.vpc_id
-    } if zone.provision_wildcard_certificate
+      bare_domain                = zone.name
+      vpc_id                     = ""
+    } # TODO if lookup(var.public_zones, zone.name, "provision_wildcard_certificate")
   }
-
 }
