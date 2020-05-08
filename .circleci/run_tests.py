2e3ef537a09178eef2f66646b126d8c4f33da93f
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-05-08 12:52:27 -0500

    Add '.hcl' to terragrunt changes list

--- .circleci/run_tests.py
+++ .circleci/run_tests.py
@@ -147,6 +147,7 @@ def any_terragrunt_files_updated(source_ref):
             '--source-ref', source_ref,
             '--target-ref', DEFAULT_TARGET_REF,
             '--terragrunt',
+            '--ext .hcl',
         ],
         stdout=subprocess.PIPE,
         check=True,
