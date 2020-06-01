e7919e797665ab5d9a805acb97494b9ff6d1a0c3
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-05-31 20:14:07 -0500

    Remove kubernetes testing from aurora and rds

--- test/aurora_test.go
+++ test/aurora_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
-	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
@@ -81,7 +80,6 @@ func TestAurora(t *testing.T) {
 	//os.Setenv("SKIP_setup", "true")
 	//os.Setenv("SKIP_deploy_terraform", "true")
 	//os.Setenv("SKIP_validate", "true")
-	//os.Setenv("SKIP_validate_kubernetes", "true")
 	//os.Setenv("SKIP_cleanup", "true")
 
 	testFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/for-learning-and-testing/data-stores/aurora")
@@ -89,9 +87,6 @@ func TestAurora(t *testing.T) {
 	defer test_structure.RunTestStage(t, "cleanup", func() {
 		terraformOptions := test_structure.LoadTerraformOptions(t, testFolder)
 		terraform.Destroy(t, terraformOptions)
-
-		kubectlOptions := test_structure.LoadKubectlOptions(t, testFolder)
-		k8s.DeleteNamespace(t, kubectlOptions, kubectlOptions.Namespace)
 	})
 
 	test_structure.RunTestStage(t, "setup", func() {
@@ -103,70 +98,35 @@ func TestAurora(t *testing.T) {
 
 		dbPassword := fmt.Sprintf("%s-%s", random.UniqueId(), random.UniqueId())
 		test_structure.SaveString(t, testFolder, "password", dbPassword)
-
-		// Create a test kubernetes namespace to deploy resources into, to avoid colliding with other tests
-		testNamespace := strings.ToLower(uniqueID)
-		test_structure.SaveString(t, testFolder, "testNamespace", testNamespace)
-		kubectlOptions := k8s.NewKubectlOptions("", "", testNamespace)
-		k8s.CreateNamespace(t, kubectlOptions, testNamespace)
-		test_structure.SaveKubectlOptions(t, testFolder, kubectlOptions)
 	})
 
 	test_structure.RunTestStage(t, "deploy_terraform", func() {
 		awsRegion := test_structure.LoadString(t, testFolder, "region")
 		uniqueID := test_structure.LoadString(t, testFolder, "uniqueID")
 		dbPassword := test_structure.LoadString(t, testFolder, "password")
-		testNamespace := test_structure.LoadString(t, testFolder, "testNamespace")
 
 		terraformOptions := createAuroraTerraformOptions(t, testFolder, awsRegion, uniqueID, dbPassword)
-		terraformOptions.Vars["create_kubernetes_service"] = true
-		terraformOptions.Vars["kubernetes_namespace"] = testNamespace
 		test_structure.SaveTerraformOptions(t, testFolder, terraformOptions)
 
 		terraform.InitAndApply(t, terraformOptions)
 	})
 
-	// We run the following validation functions in parallel by using subtests. However, subtest parallelization has a
-	// quirk where it will not wait for all the tests to finish by default. This could cause this test routine to start
-	// running the cleanup functions while the validation functions are running.
-	// The way to solve this is to use a subtest that is run in serial to group the parallel subtests.
-	t.Run("validation", func(t *testing.T) {
+	test_structure.RunTestStage(t, "validate", func() {
 		dbName := "aurora"
 		dbUsername := "aurora"
 		dbPassword := test_structure.LoadString(t, testFolder, "password")
 		terraformOptions := test_structure.LoadTerraformOptions(t, testFolder)
-		clusterName := terraformOptions.Vars["name"]
 		dbEndpoint := terraform.OutputRequired(t, terraformOptions, "primary_endpoint")
 		dbPort := terraform.OutputRequired(t, terraformOptions, "port")
 
-		t.Run("local", func(t *testing.T) {
-			t.Parallel()
-			test_structure.RunTestStage(t, "validate", func() {
-				info := RDSInfo{
-					Username:   dbUsername,
-					Password:   dbPassword,
-					DBName:     dbName,
-					DBEndpoint: dbEndpoint,
-					DBPort:     dbPort,
-				}
-				smokeTestMysql(t, info)
-			})
-		})
-
-		t.Run("kubernetes", func(t *testing.T) {
-			t.Parallel()
-			test_structure.RunTestStage(t, "validate_kubernetes", func() {
-				kubectlOptions := test_structure.LoadKubectlOptions(t, testFolder)
-				info := RDSInfo{
-					Username:   dbUsername,
-					Password:   dbPassword,
-					DBName:     dbName,
-					DBEndpoint: fmt.Sprintf("%s.%s.svc.cluster.local", clusterName, kubectlOptions.Namespace),
-					DBPort:     dbPort,
-				}
-				smokeTestMysqlWithKubernetes(t, kubectlOptions, info)
-			})
-		})
+		info := RDSInfo{
+			Username:   dbUsername,
+			Password:   dbPassword,
+			DBName:     dbName,
+			DBEndpoint: dbEndpoint,
+			DBPort:     dbPort,
+		}
+		smokeTestMysql(t, info)
 	})
 }
 
