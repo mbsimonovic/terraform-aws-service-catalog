d18e4540312474649bfcebcac0cf0038fa37c129
Zack Proser <zackproser@gmail.com>
2020-06-01 15:50:53 -0700

    Data source index should be zero

--- modules/mgmt/bastion-host/main.tf
+++ modules/mgmt/bastion-host/main.tf
@@ -30,7 +30,7 @@ module "bastion" {
   vpc_id    = var.vpc_id
   subnet_id = var.subnet_id
 
-  dns_zone_id = var.create_dns_record ? data.aws_route53_zone.selected[1].zone_id : ""
+  dns_zone_id = var.create_dns_record ? data.aws_route53_zone[0].selected.zone_id : ""
   dns_name    = var.domain_name
   dns_type    = "A"
   dns_ttl     = "300"
