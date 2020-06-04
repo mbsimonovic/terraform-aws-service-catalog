754034e4295e9812d2a46611d4b5e30a50642d52
Zack Proser <zackproser@gmail.com>
2020-06-03 22:15:46 -0700

    Update required providers. Remove backend block

--- modules/services/ecs-cluster/main.tf
+++ modules/services/ecs-cluster/main.tf
@@ -3,31 +3,17 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ---------------------------------------------------------------------------------------------------------------------
-# CONFIGURE OUR AWS CONNECTION
-# ---------------------------------------------------------------------------------------------------------------------
-
-provider "aws" {
-  # The AWS region in which all resources will be created
-  region = var.aws_region
-
-  # Provider version 2.X series is the latest, but has breaking changes with 1.X series.
-  version = "~> 2.6"
-
-  # Only these AWS Account IDs may be operated on by this template
-  allowed_account_ids = [var.aws_account_id]
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # CONFIGURE REMOTE STATE STORAGE
 # ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
+  # Require at least 0.12.6, which added for_each support; make sure we don't accidentally pull in 0.13.x, as that may
+  # have backwards incompatible changes when it comes out.
+  required_version = "~> 0.12.6"
 
-  # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
-  # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-  required_version = "= 0.12.17"
+  required_providers {
+    aws = "~> 2.6"
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
