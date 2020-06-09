dad15c0e85d1d8c7367ff431916d3d350acf8677
Zack Proser <zackproser@gmail.com>
2020-06-09 14:09:21 -0700

    Continue building out ecs-cluster module

--- modules/services/ecs-cluster/main.tf
+++ modules/services/ecs-cluster/main.tf
@@ -31,21 +31,10 @@ module "ecs_cluster" {
   vpc_id                            = var.vpc_id
   vpc_subnet_ids                    = var.vpc_subnet_ids
   tenancy                           = var.tenancy
-  allow_ssh                         = var.allow_ssh
-  allow_ssh_from_security_group_ids = [var.openvpn_server_security_group_id]
+  allow_ssh_from_security_group_ids = var.allow_ssh_from_security_group_ids
 
-  alb_security_group_ids     = compact(concat(data.template_file.alb_public_security_group_ids.*.rendered, data.template_file.alb_internal_security_group_ids.*.rendered))
+  alb_security_group_ids     = compact(concat(var.internal_alb_sg_id, var.public_alb_sg_id))
   num_alb_security_group_ids = (var.allow_requests_from_public_alb ? 1 : 0) + (var.allow_requests_from_internal_alb ? 1 : 0)
-}
-
-data "template_file" "alb_public_security_group_ids" {
-  count    = var.allow_requests_from_public_alb ? 1 : 0
-  template = var.alb_security_group_ids[count.index]
-}
-
-data "template_file" "alb_internal_security_group_ids" {
-  count    = var.allow_requests_from_internal_alb ? 1 : 0
-  template = var.alb_security_group_ids[count.index]
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -54,13 +43,53 @@ data "template_file" "alb_internal_security_group_ids" {
 # Docker registry.
 # ---------------------------------------------------------------------------------------------------------------------
 
+locals {
+  # Default cloud init script for this module 
+  cloud_init = {
+    filename = "ecs-cluster-default-cloud-init"
+    content_type = "text/x-shellscript"
+    content = data.template_file.user_data.rendered
+  }
+
+  # Merge in all the cloud init scripts the user has passed in 
+  cloud_init_parts = merge({ default: local.cloud_init }, var.cloud_init_parts)
+}
+
 data "template_file" "user_data" {
-  template = file("${path.module}/user-data/user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
-    aws_region       = var.aws_region
-    ecs_cluster_name = var.cluster_name
-    log_group_name   = var.cluster_name
+    log_group_name = var.cluster_name 
+    enable_cloudwatch_log_aggregation = var.enable_cloudwatch_log_aggregation
+    enable_ssh_grunt = var.enable_ssh_grunt 
+    enable_fail2ban = var.enable_fail2ban
+    enable_ip_lockdown = var.enable_ip_lockdown
+    ssh_grunt_iam_group = var.ssh_grunt_iam_group 
+    ssh_grunt_iam_group_sudo = var.ssh_grunt_iam_group_sudo
+    external_account_ssh_grunt_role_arn = var.external_account_ssh_grunt_role_arn
+    enable_datadog_task = var.enable_datadog_task
+    datadog_task_arn = var.data_dog_task_arn 
+    datadog_api_key_secret = var.data_dog_api_key_secret
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ADD IAM POLICY THAT ALLOWS EACH ECS NODE TO LAUNCH THE DATA DOG AGENT 
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_iam_role_policy" "data_dog_iam_permissions" {
+  count = var.enable_datadog_task ? 1 : 0 
+
+  name = "data-dog-iam-permissions"
+  role = module.ecs_cluster.ecs_instance_iam_role_name 
+  policy = data.aws_iam_policy_document.data_dog_iam_permissions.json
+}
+
+data "aws_iam_policy_document" "data_dog_iam_permissions" {
+  statement {
+    effect = "Allow"
+    actions = ["ecs:StartTask"]
+    resources = ["*"]
   }
 }
 
@@ -69,12 +98,10 @@ data "template_file" "user_data" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "cloudwatch_log_aggregation" {
-  # TODO - replace correct ref after branch is merged 
-  # source      = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/logs/cloudwatch-log-aggregation-iam-policy?ref=v0.13.2"
   source      = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/logs/cloudwatch-log-aggregation-iam-policy?ref=v0.21.2"
   name_prefix = var.cluster_name
 
-  # not create a standalone IAM policy with that JSON. We'll instead add that JSON to the ECS cluster IAM role.
+  # We set this to false so that the cloudwatch-custom-metrics-iam policy generates JSON for the policy, but does not create a standalone IAM policy with that JSON. We'll instead add that JSON to the ECS cluster IAM role. 
   create_resources = false
 }
 
@@ -118,11 +145,10 @@ resource "aws_iam_role_policy" "custom_cloudwatch_metrics" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "ecs_cluster_cpu_memory_alarms" {
-  source               = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/alarms/ecs-cluster-alarms?ref=v0.13.2"
+  source               = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/alarms/ecs-cluster-alarms?ref=v0.21.2"
   ecs_cluster_name     = var.cluster_name
-  alarm_sns_topic_arns = [var.alarms_sns_topic_arn]
+  alarm_sns_topic_arns = var.alarms_sns_topic_arn
 
-  # These alarms will go off if CPU or memory usage is over 90 percent during a 5 minute period
   high_cpu_utilization_threshold    = var.high_cpu_utilization_threshold
   high_cpu_utilization_period       = var.high_cpu_utilization_period
   high_memory_utilization_threshold = var.high_memory_utilization_threshold
@@ -130,7 +156,7 @@ module "ecs_cluster_cpu_memory_alarms" {
 }
 
 module "ecs_cluster_disk_memory_alarms" {
-  source               = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/alarms/asg-disk-alarms?ref=v0.13.2"
+  source               = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/alarms/asg-disk-alarms?ref=v0.21.2"
   asg_names            = [module.ecs_cluster.ecs_cluster_asg_name]
   num_asg_names        = 1
   alarm_sns_topic_arns = [var.alarms_sns_topic_arn]
@@ -138,13 +164,12 @@ module "ecs_cluster_disk_memory_alarms" {
   file_system = "/dev/xvda1"
   mount_path  = "/"
 
-  # These alarms will go off if disk space usage is over 90 percent during a 5 minute period
   high_disk_utilization_threshold = var.high_disk_utilization_threshold
   high_disk_utilization_period    = var.high_disk_utilization_period
 }
 
 module "metric_widget_ecs_cluster_cpu_usage" {
-  source = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/metrics/cloudwatch-dashboard-metric-widget?ref=v0.13.2"
+  source = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/metrics/cloudwatch-dashboard-metric-widget?ref=v0.21.2"
 
   period = 60
   stat   = "Average"
@@ -156,7 +181,7 @@ module "metric_widget_ecs_cluster_cpu_usage" {
 }
 
 module "metric_widget_ecs_cluster_memory_usage" {
-  source = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/metrics/cloudwatch-dashboard-metric-widget?ref=v0.13.2"
+  source = "git::git@github.com:gruntwork-io/module-aws-monitoring.git//modules/metrics/cloudwatch-dashboard-metric-widget?ref=v0.21.2"
 
   period = 60
   stat   = "Average"
@@ -166,6 +191,126 @@ module "metric_widget_ecs_cluster_memory_usage" {
     ["AWS/ECS", "MemoryUtilization", "ClusterName", var.cluster_name],
   ]
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# BASE RESOURCES 
+# Includes resources common to all EC2 instances in the Service Catalog, including permissions for ssh-grunt, CloudWatch Logs aggregation, CloudWatch metrics, and CloudWatch alarms 
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "ec2_baseline" {
+  source = "../../base/ec2-baseline"
+
+  name = var.cluster_name
+  enable_ssh_grunt = var.enable_ssh_grunt 
+  external_account_ssh_grunt_role_arn = var.external_account_ssh_grunt_role_arn 
+  enable_cloudwatch_log_aggregation = var.enable_cloudwatch_log_aggregation
+  # We use custom metrics for ECS, as specified above 
+  enable_cloudwatch_metrics = false
+  iam_role_arn = module.ecs_cluster.ecs_instance_iam_role_name
+  cloud_init_parts = local.cloud_init_parts
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ADD AN AUTO SCALING POLICY TO ADD MORE INSTANCES WHEN CPU USAGE IS HIGH 
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_autoscaling_policy" "scale_out" {
+  count = var.enable_autoscaling ? 1 : 0 
+
+  name = "${var.cluster_name}-scale-out"
+  autoscaling_group_name = module.ecs_cluster.ecs_cluster_asg_name 
+  adjustment_type = "ChangeInCapacity"
+  policy_type = "StepScaling"
+  estimated_instance_warmup = 200 
+
+  # Each of the step_adjustment values below defines what to do if the metric is a given amount above the alarm 
+  # threshold. Since our threshold is set at 75, the values below define what to do for CPU usage between 75 and 85% 
+  # and then anything above 85% 
+  
+  step_adjustment {
+    metric_interval_lower_bound = 0.0
+    metric_interval_upper_bound = 10.0
+    scaling_adjustment = 1 
+  }
+
+  step_adjustment {
+    metric_interval_lower_bound = 10.0 
+    scaling_adjustment = 2 
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
+  count = var.enable_autoscaling ? 1 : 0 
+
+  alarm_name = "${var.cluster_name}-high-cpu-utilization"
+  alarm_description = "An alarm that goes off if the CPU usage in the ${var.cluster_name} ECS cluster is high"
+  namespace = "AWS/EC2"
+  metric_name = "CPUUtilization"
+  dimensions = {
+    AutoScalingGroupName = module.ecs_cluster.ecs_cluster_asg_name 
+  }
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "3"
+  period = "60"
+  statistic = "Average"
+  threshold = "75"
+  unit = "Percent"
+  alarm_actions = [aws_autoscaling_policy.scale_out.arn]
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ADD AN AUTO SCALING POLICY TO REMOVE INSTANCES WHEN CPU USAGE IS LOW 
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_autoscaling_policy" "scale_in" {
+  count = var.enable_autoscaling ? 1 : 0 
+
+  name = "${var.cluster_name}-scale-in"
+  autoscaling_group_name = module.ecs_cluster.ecs_cluster_asg_name 
+  adjustment_type = "ChangeInCapacity"
+  policy_type = "SimpleScaling"
+  scaling_adjustment = -1
+  cooldown = 200
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_utilization" {
+  count = var.enable_autoscaling ? 1 : 0
+  
+  alarm_name = "${var.cluster_name}-low-cpu-utilization"
+  alarm_description = "An alarm that goes off if the CPU usage in the ${var.cluster_name} ECS cluster is low"
+  namespace = "AWS/EC2"
+  metric_name = "CPUUtilization"
+  dimensions = {
+    AutoScalingGroup = module.ecs_cluster.ecs_cluster_asg_name
+  }
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods = "10"
+  period = "60"
+  statistic = "Average"
+  threshold = "50"
+  unit = "Percent"
+  alarm_actions = [aws_autoscaling_policy.scale_in.arn]
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ENABLE ACCESS TO CERTAIN PORTS WITHIN THE ECS CLUSTER 
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group_rule" "cluster_access" {
+  count = length(var.enable_cluster_access_ports) * length(var.cluster_access_from_sgs)
+
+  type = "ingress"
+  from_port = local.product[count.index][0]
+  to_port = local.product[count.index][0]
+  protocol = "tcp"
+  source_security_group_id = local.product[count.index][1]
+  security_group_id = module.ecs_cluster.ecs_instance_security_group_id
+}
+
+locals {
+  product = setproduct(var.enable_cluster_access_ports, var.cluster_access_from_sgs)
+}
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # GIVE SSH-GRUNT PERMISSIONS TO TALK TO IAM
@@ -195,3 +340,10 @@ resource "aws_iam_role_policy" "ssh_grunt_permissions" {
 }
 
 
+# ---------------------------------------------------------------------------------------------------------------------
+# GET INFO ABOUT CURRENT USER/ACCOUNT/REGION
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
