5380c76d232db9fcc50856963ed8fcdc9f813241
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-06-09 19:51:04 -0500

    Add support for setting up AWS CloudMap in route53 module

---
+++ examples/for-learning-and-testing/networking/cloudmap/main.tf
@@ -0,0 +1,119 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY CLOUD MAP NAMESPACES FOR SERVICE DISCOVERY
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "route53" {
+  # When using these modules in your own repos, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/networking/route53?ref=v1.2.3"
+  source = "../../../../modules/networking/route53"
+
+  service_discovery_public_namespaces  = var.service_discovery_public_namespaces
+  service_discovery_private_namespaces = var.service_discovery_private_namespaces
+}
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY AN EC2 INSTANCE TO REGISTER TO THE NAMESPACE
+# The following is an example and test for how to register an EC2 instance to the Service Discovery Namespace as a
+# service. This will:
+# - Create a Service Discovery Service to hold all the instances for the bastion
+# - Create an EC2 instance that will register itself to the Service Discovery Service using it's public IP.
+# - Make sure the EC2 instance allows SSH and has IAM permissions to register itself to AWS CloudMap.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+resource "aws_service_discovery_service" "bastion" {
+  name = var.test_instance_name
+
+  dns_config {
+    namespace_id = (
+      contains(keys(module.route53.service_discovery_public_namespaces), var.test_instance_namespace)
+      ? module.route53.service_discovery_public_namespaces[var.test_instance_namespace].id
+      : module.route53.service_discovery_private_namespaces[var.test_instance_namespace].id
+    )
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = data.aws_ami.al2.id
+  instance_type               = "t3.micro"
+  key_name                    = var.test_instance_key_pair
+  subnet_id                   = var.test_instance_vpc_subnet_id
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
+  iam_instance_profile        = aws_iam_instance_profile.test_profile.name
+  associate_public_ip_address = true
+  user_data                   = <<EOF
+#!/bin/bash
+# Send the log output from this script to user-data.log, syslog, and the console
+# From: https://alestic.com/2010/12/ec2-user-data-output/
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+instance_ipv4="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+instance_id="$(curl http://169.254.169.254/latest/meta-data/instance-id)"
+service_id="${aws_service_discovery_service.bastion.id}"
+echo "Registering $instance_ipv4 to service $service_id as $instance_id"
+
+aws servicediscovery register-instance \
+  --region ${var.aws_region} \
+  --service-id  "$service_id" \
+  --instance-id "$instance_id" \
+  --attributes AWS_INSTANCE_IPV4="$instance_ipv4"
+EOF
+
+  tags = {
+    Name = var.test_instance_name
+  }
+
+  depends_on = [aws_iam_role_policy.register_permissions]
+}
+
+resource "aws_security_group" "bastion" {
+  vpc_id = data.aws_subnet.selected.vpc_id
+
+  # Inbound SSH from world
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound all
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.test_instance_name}-bastion"
+  }
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "${var.test_instance_name}-register-permissions"
+  role = aws_iam_role.register_permissions.name
+}
+
+resource "aws_iam_role" "register_permissions" {
+  name               = "${var.test_instance_name}-register-permissions"
+  assume_role_policy = data.aws_iam_policy_document.assume_from_ec2.json
+}
+
+resource "aws_iam_role_policy" "register_permissions" {
+  name   = "${var.test_instance_name}-register-permissions"
+  role   = aws_iam_role.register_permissions.id
+  policy = data.aws_iam_policy_document.register_permissions.json
+}
