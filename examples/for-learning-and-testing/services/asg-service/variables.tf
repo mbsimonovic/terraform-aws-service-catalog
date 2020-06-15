64a6a810bd24fcef9b4ab6409fa6d1e3c146f16c
Marina Limeira <marinaflessa@gmail.com>
2020-06-15 16:41:58 +0200

    :nail_care: Add description for variables

--- examples/for-learning-and-testing/services/asg-service/variables.tf
+++ examples/for-learning-and-testing/services/asg-service/variables.tf
@@ -1,10 +1,20 @@
 variable "aws_region" {
-
+  description = "The AWS region in which all resources will be created."
+  type        = string
 }
 
 variable "name" {
-  type = string
-  default = "marina-testing"
+  description = "The name to use for the ASG and all other resources created by these templates"
+  type        = string
 }
 
-variable "ami" {}
+variable "ami" {
+  description = "The ID of the AMI to run on each instance in the ASG"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "An SSH Key Pair that can be used to connect to the EC2 Instance. This can be omitted entirely (set to an empty string) or used as an emergency backup mechanism in case ssh-grunt isn't working."
+  type        = string
+  default     = null
+}
