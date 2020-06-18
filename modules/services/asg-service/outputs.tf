7caa7f645c64d1e4721cb5edf074bc85c4f2439c
Marina Limeira <marinaflessa@gmail.com>
2020-06-18 11:02:03 +0200

    Add more outputs to module

--- modules/services/asg-service/outputs.tf
+++ modules/services/asg-service/outputs.tf
@@ -2,14 +2,26 @@ output "asg_name" {
   value = module.asg.asg_name
 }
 
+output "asg_unique_id" {
+  value = module.asg.asg_unique_id
+}
+
 output "fully_qualified_domain_name" {
   value = element(concat(aws_route53_record.service.*.fqdn, [""]), 0)
 }
 
-//output "alb_dns_name" {
-//  value = data.terraform_remote_state.alb.outputs.alb_dns_name
-//}
-//
-//output "alb_name" {
-//  value = data.terraform_remote_state.alb.outputs.alb_name
-//}
+output "security_group_id" {
+  value = aws_security_group.lc_security_group.id
+}
+
+output "alb_listener_rule_id" {
+  value = aws_alb_listener_rule.paths_to_route_to_this_service.id
+}
+
+output "launch_configuration_id" {
+  value = aws_launch_configuration.launch_configuration.id
+}
+
+output "launch_configuration_name" {
+  value = aws_launch_configuration.launch_configuration.id
+}
