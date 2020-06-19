7a63f1dd0d19a370b1fdc2ee2f090d5798adacea
Zack Proser <zackproser@gmail.com>
2020-06-18 21:19:44 -0700

    Clean up duplicated functions

--- modules/services/ecs-cluster/install.sh
+++ modules/services/ecs-cluster/install.sh
@@ -47,24 +47,6 @@ function include_ec2_baseline {
   source $EC2_BASELINE_PATH/install.sh
 }
 
-function install_security_packages {
-  local -r module_security_version="$1"
-  local -r enable_ssh_grunt="$2"
-
-  echo "Installing Gruntwork Security Modules"
-
-  gruntwork-install --module-name 'auto-update' --repo https://github.com/gruntwork-io/module-security --tag "$module_security_version"
-  gruntwork-install --module-name 'fail2ban' --repo https://github.com/gruntwork-io/module-security --tag "$module_security_version"
-  gruntwork-install --module-name 'ntp' --repo https://github.com/gruntwork-io/module-security --tag "$module_security_version"
-  gruntwork-install --module-name 'ip-lockdown' --repo https://github.com/gruntwork-io/module-security --tag "$module_security_version"
-
-  if [[ "$enable_ssh_grunt" == "true" ]]; then
-    gruntwork-install --binary-name 'ssh-grunt' --repo https://github.com/gruntwork-io/module-security --tag "$module_security_version"
-
-    sudo /usr/local/bin/ssh-grunt iam install --iam-group ssh-grunt-users --iam-group-sudo ssh-grunt-sudo-users --role-arn "arn:aws:iam::087285199408:role/allow-ssh-grunt-access-from-other-accounts"
-  fi
-}
-
 function install_monitoring_packages {
   local -r module_aws_monitoring_version="$1"
   local -r enable_cloudwatch_metrics="$2"
@@ -89,33 +71,6 @@ function install_monitoring_packages {
   fi
 
   gruntwork-install --module-name 'logs/syslog' --repo https://github.com/gruntwork-io/module-aws-monitoring --tag "$module_aws_monitoring_version"
-}
-
-function assert_not_empty {
-  local -r arg_name="$1"
-  local -r arg_value="$2"
-
-  if [[ -z "$arg_value" ]]; then
-    log_error "The value for '$arg_name' cannot be empty."
-    exit 1
-  fi
-}
-
-function assert_env_var_not_empty {
-  local -r var_name="$1"
-  local -r var_value="${!var_name}"
-
-  if [[ -z "$var_value" ]]; then
-    echo "ERROR: Required environment variable $var_name not set."
-    exit 1
-  fi
-}
-
-function install_bash_commons {
-  local -r bash_commons_version="$1"
-
-  echo "Installing bash-commons"
-  gruntwork-install --module-name 'bash-commons' --repo https://github.com/gruntwork-io/bash-commons --tag "$bash_commons_version"
 }
 
 function install_terraform {
@@ -190,12 +145,6 @@ function install_git {
   sudo apt-get install -y git
 }
 
-function install_aws_cli {
-  echo "Installing AWS CLI"
-  sudo apt-get install -y jq python-pip unzip curl
-  sudo pip install awscli
-}
-
 function install_build_dependencies {
   local -r terraform_version="$1"
   local -r terragrunt_version="$2"
@@ -209,34 +158,14 @@ function install_build_dependencies {
   install_git
 }
 
-function install_gruntwork_modules {
-  local -r module_security_version="$1"
-  local -r module_aws_monitoring_version="$2"
-  local -r bash_commons_version="$3"
-  local -r enable_ssh_grunt="$4"
-  local -r enable_cloudwatch_metrics="$5"
-  local -r enable_cloudwatch_log_aggregation="${6}"
-
-  install_aws_cli
-  install_bash_commons "$bash_commons_version"
-  install_security_packages "$module_security_version" "$enable_ssh_grunt"
-  install_monitoring_packages "$module_aws_monitoring_version" "$enable_cloudwatch_metrics" "$enable_cloudwatch_log_aggregation"
-}
-
 function install_ecs_cluster {
   # Read from env vars to make it easy to set these in a Packer template (without super-wide --module-param foo=bar code).
   # Fallback to default version if the env var is not set.
   local ecs_cluster_version="${ecs_cluster_version:-DEFAULT_ECS_CLUSTER_VERSION}"
-  local module_security_version="${module_security_version:-DEFAULT_MODULE_SECURITY_VERSION}"
-  local module_aws_monitoring_version="${module_aws_monitoring_version:-DEFAULT_MODULE_AWS_MONITORING_VERSION}"
-  local bash_commons_version="${bash_commons_version:-DEFAULT_BASH_COMMONS_VERSION}"
   local terraform_version="${terraform_version:-DEFAULT_TERRAFORM_VERSION}"
   local terragrunt_version="${terragrunt_version:-DEFAULT_TERRAGRUNT_VERSION}"
   local packer_version="${packer_version:-DEFAULT_PACKER_VERSION}"
   local docker_version="${docker_version:-DEFAULT_DOCKER_VERSION}"
-  local enable_ssh_grunt="${enable_ssh_grunt:-DEFAULT_ENABLE_SSH_GRUNT}"
-  local enable_cloudwatch_metrics="${enable_cloudwatch_metrics:-DEFAULT_ENABLE_CLOUDWATCH_METRICS}"
-  local enable_cloudwatch_log_aggregation="${enable_cloudwatch_log_aggregation:-DEFAULT_ENABLE_CLOUDWATCH_LOG_AGGREGATION}"
 
   while [[ $# > 0 ]]; do
     local key="$1"
@@ -247,32 +176,7 @@ function install_ecs_cluster {
         ecs_cluster_version="$2"
         shift
         ;;
-      --module-security-version)
-        assert_not_empty "$key" "$2"
-        module_security_version="$2"
-        shift
-        ;;
-      --module-aws-monitoring-version)
-        assert_not_empty "$key" "$2"
-        module_aws_monitoring_version="$2"
-        shift
-        ;;
-      --module-stateful-server-version)
-        assert_not_empty "$key" "$2"
-        module_stateful_server_version="$2"
-        shift
-        ;;
-      --module-ci-version)
-        assert_not_empty "$key" "$2"
-        module_ci_version="$2"
-        shift
-        ;;
-     --bash-commons-version)
-        assert_not_empty "$key" "$2"
-        bash_commons_version="$2"
-        shift
-        ;;
-      --terraform-version)
+  --terraform-version)
         assert_not_empty "$key" "$2"
         terraform_version="$2"
         shift
@@ -292,11 +196,7 @@ function install_ecs_cluster {
         docker_version="$2"
         shift
         ;;
-      --enable-ssh-grunt)
-        enable_ssh_grunt="$2"
-        shift
-        ;;
-      --enable-cloudwatch-metrics)
+     --enable-cloudwatch-metrics)
         enable_cloudwatch_metrics="$2"
         shift
         ;;
@@ -315,21 +215,13 @@ function install_ecs_cluster {
 
   assert_env_var_not_empty "GITHUB_OAUTH_TOKEN"
 
-  install_gruntwork_modules \
-    "$module_security_version" \
-    "$module_aws_monitoring_version" \
-    "$bash_commons_version" \
-    "$enable_ssh_grunt" \
-    "$enable_cloudwatch_metrics" \
-    "$enable_cloudwatch_log_aggregation"
-
   install_build_dependencies \
     "$terraform_version" \
     "$terragrunt_version" \
     "$packer_version" \
     "$docker_version"
 
-    gruntwork-install --module-name 'ecs-scripts' --repo https://github.com/gruntwork-io/module-ecs --tag "$ecs_cluster_version"
+  gruntwork-install --module-name 'ecs-scripts' --repo https://github.com/gruntwork-io/module-ecs --tag "$ecs_cluster_version"
 }
 
 include_ec2_baseline
