c69ab0da08e0cb8c16acbb5c549b3e3b1f7d2f79
Marina Limeira <marinaflessa@gmail.com>
2020-06-20 09:32:46 +0200

    Add install.sh

---
+++ modules/services/asg-service/install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Install Gruntwork dependencies on a Linux server
+
+set -e
+
+# TODO: Update ref to a tag when released
+readonly DEFAULT_EC2_BASELINE_REF="master"
+
+# You can set the version of the build tooling to this value to skip installing it
+readonly SKIP_INSTALL_VERSION="NONE"
+
+# NOTE: A few variables will be imported from ec2-baseline
+# - DEFAULT_MODULE_SECURITY_VERSION
+# - DEFAULT_MODULE_AWS_MONITORING_VERSION
+# - DEFAULT_BASH_COMMONS_VERSION
+# - DEFAULT_ENABLE_SSH_GRUNT
+# - DEFAULT_ENABLE_CLOUDWATCH_LOG_AGGREGATION
+# - DEFAULT_ENABLE_CLOUDWATCH_METRICS
+
+function include_ec2_baseline {
+  gruntwork-install \
+    --module-name base/ec2-baseline \
+    --repo https://github.com/gruntwork-io/aws-service-catalog \
+    --tag ${DEFAULT_EC2_BASELINE_REF}
+
+  # Include common defaults and functions from the ec2-baseline install script
+  # See: https://github.com/gruntwork-io/aws-service-catalog/blob/master/modules/base/ec2-baseline
+  readonly EC2_BASELINE_RELATIVE_PATH="../../base/ec2-baseline"
+  readonly EC2_BASELINE_PATH="$(dirname $(realpath $0))/${EC2_BASELINE_RELATIVE_PATH}"
+  if [[ ! -f "${EC2_BASELINE_PATH}/install.sh" ]]; then
+    echo "ERROR: $EC2_BASELINE_PATH/install.sh not found."
+    exit 1
+  fi
+
+  source $EC2_BASELINE_PATH/install.sh
+}
+
+include_ec2_baseline
