7c68ef4d0797b5be797503055707da13132fa5aa
Marina Limeira <marinaflessa@gmail.com>
2020-06-22 19:11:26 +0200

    Add ssh to ingress rules

--- modules/services/asg-service/main.tf
+++ modules/services/asg-service/main.tf
@@ -106,14 +106,14 @@ resource "aws_security_group_rule" "ingress_alb" {
   security_group_id = aws_security_group.lc_security_group.id
 }
 
-//resource "aws_security_group_rule" "ingress_ssh" {
-//  type              = "ingress"
-//  from_port         = 22
-//  to_port           = 22
-//  protocol          = "tcp"
-//  cidr_blocks       = ["0.0.0.0/0"]
-//  security_group_id = aws_security_group.lc_security_group.id
-//}
+resource "aws_security_group_rule" "ingress_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lc_security_group.id
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # BASE RESOURCES
