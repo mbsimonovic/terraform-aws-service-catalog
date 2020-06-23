0a072dec8b735b9669126472876d8d03122926b1
rho song <rhomsang@gmail.com>
2020-06-23 13:12:07 -0700

    Create static-website module tested with a terragrunt.hcl

---
+++ modules/services/static-website/outputs.tf
@@ -0,0 +1,24 @@
+output "cloudfront_domain_names" {
+  description = ""
+  value = [module.cloudfront.cloudfront_domain_names]
+}
+
+output "cloudfront_id" {
+  description = ""
+  value = module.cloudfront.cloudfront_id
+}
+
+output "website_s3_bucket_arn" {
+  description = ""
+  value = module.static_website.website_bucket_arn
+}
+
+output "website_access_logs_bucket_arn" {
+  description = ""
+  value = module.static_website.access_logs_bucket_arn
+}
+
+output "cloudfront_access_logs_bucket_arn" {
+  description = ""
+  value = module.cloudfront.access_logs_bucket_arn
+}
