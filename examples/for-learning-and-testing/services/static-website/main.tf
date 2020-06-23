0530d22221f1a33a8bf1a167ba4b0618ccd002a9
rho song <rhomsang@gmail.com>
2020-06-23 13:12:28 -0700

    Start writing an example using static-website module

---
+++ examples/for-learning-and-testing/services/static-website/main.tf
@@ -0,0 +1,37 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# DEPLOY A STATIC WEBSITE WITH CLOUDFRONT
+# ----------------------------------------------------------------------------------------------------------------------
+provider "aws" {
+  region = var.aws_region
+  version = "~> 2.6"
+}
+
+module "static_website" {
+  source = "../../../../modules/services/static-website"
+
+  aws_region                    = var.aws_region
+  aws_account_id                = "087285199408"
+  website_domain_name           = "acme-stage-static.gruntwork.in"
+  create_route53_entry          = true
+  terraform_state_region        = "us-east-1"
+  terraform_state_s3_bucket     = "rho-test-static-website_state"
+  terraform_state_aws_region    = "us-east-1"
+  terraform_state_aws_s3_bucket = "rho-test-static-website"
+  acm_certificate_domain_name   = "*.gruntwork.in"
+  hosted_zone_id                = "Z1Y6DCUKW424UT"
+
+# CloudFront cache settings
+# These are the default values. Change them and uncomment if needed.
+#  default_ttl = 30
+#  max_ttl     = 60
+#  min_ttl     = 0
+
+# Only set this to true if, when running 'terragrunt destroy,' you want to delete the contents of the S3 buckets that
+# store the website, redirects, and access logs. Note that you must set this to true and run 'terragrunt apply' FIRST,
+# before running 'destroy'!
+  force_destroy = true
+}
+
+# Should this example use ../../networking/route53?
+# No, because we don't need this example to create the hosted zones.
+# We only need it to use the existing ones.
---
+++ examples/for-learning-and-testing/services/static-website/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+}
+
+variable "" {
+}
