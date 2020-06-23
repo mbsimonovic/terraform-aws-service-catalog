e026b357babffd5452d5aa5e886c59f6c8197105
Marina Limeira <marinaflessa@gmail.com>
2020-06-23 13:39:01 +0200

    Install dependencies -- based on bastion-host/install.sh

--- modules/services/asg-service/install.sh
+++ modules/services/asg-service/install.sh
@@ -35,7 +35,71 @@ function include_ec2_baseline {
   source $EC2_BASELINE_PATH/install.sh
 }
 
+function install_host {
+  # Read from env vars to make it easy to set these in a Packer template (without super-wide --module-param foo=bar code).
+  # Fallback to default version if the env var is not set.
+  local module_security_version="${module_security_version:-$DEFAULT_MODULE_SECURITY_VERSION}"
+  local module_aws_monitoring_version="${module_aws_monitoring_version:-$DEFAULT_MODULE_AWS_MONITORING_VERSION}"
+  local bash_commons_version="${bash_commons_version:-$DEFAULT_BASH_COMMONS_VERSION}"
+  local enable_ssh_grunt="${enable_ssh_grunt:-$DEFAULT_ENABLE_SSH_GRUNT}"
+  local enable_cloudwatch_metrics="${enable_cloudwatch_metrics:-$DEFAULT_ENABLE_CLOUDWATCH_METRICS}"
+  local enable_cloudwatch_log_aggregation="${enable_cloudwatch_log_aggregation:-$DEFAULT_ENABLE_CLOUDWATCH_LOG_AGGREGATION}"
+
+  while [[ $# -gt 0 ]]; do
+    local key="$1"
+
+    case "$key" in
+      --module-security-version)
+        assert_not_empty "$key" "$2"
+        module_security_version="$2"
+        shift
+        ;;
+      --module-aws-monitoring-version)
+        assert_not_empty "$key" "$2"
+        module_aws_monitoring_version="$2"
+        shift
+        ;;
+      --bash-commons-version)
+        assert_not_empty "$key" "$2"
+        bash_commons_version="$2"
+        shift
+        ;;
+      --enable-ssh-grunt)
+        enable_ssh_grunt="$2"
+        shift
+        ;;
+      --enable-cloudwatch-metrics)
+        enable_cloudwatch_metrics="$2"
+        shift
+        ;;
+      --enable-cloudwatch-log-aggregation)
+        enable_cloudwatch_log_aggregation="$2"
+        shift
+        ;;
+      *)
+        echo "ERROR: Unrecognized argument: $key"
+        exit 1
+        ;;
+    esac
+
+    shift
+  done
+
+  assert_env_var_not_empty "GITHUB_OAUTH_TOKEN"
+
+  install_gruntwork_modules \
+    "$bash_commons_version" \
+    "$module_security_version" \
+    "$module_aws_monitoring_version" \
+    "$enable_ssh_grunt" \
+    "$enable_cloudwatch_metrics" \
+    "$enable_cloudwatch_log_aggregation"
+
+  install_user_data \
+    "${EC2_BASELINE_PATH}/user-data-common.sh"
+
+}
+
 include_ec2_baseline
 
-install_user_data \
-    "${EC2_BASELINE_PATH}/user-data-common.sh"
+install_host "$@"
