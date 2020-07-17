25912c13bb5c8c116809a5dc1be9079b6bc3c0d6
Zack Proser <zackproser@gmail.com>
2020-07-17 09:20:34 -0700

    Clean up variables a bit

--- examples/for-learning-and-testing/services/ecs-service/main.tf
+++ examples/for-learning-and-testing/services/ecs-service/main.tf
@@ -12,29 +12,44 @@ module "application" {
   # source = "git::git@github.com:gruntwork-io/aws-service-catalog.git//modules/services/k8s-service?ref=v1.0.8"
   source = "../../../../modules/services/ecs-service"
 
-  cpu                               = var.cpu
-  aws_region                        = var.aws_region
-  service_name                      = var.service_name
-  desired_number_of_tasks           = var.desired_number_of_tasks
-  max_number_of_tasks               = var.max_number_of_tasks
-  canary_version                    = var.canary_version
-  ecs_node_port_mappings            = var.ecs_node_port_mappings
-  vpc_env_var_name                  = var.vpc_env_var_name
+  aws_region       = var.aws_region
+  service_name     = var.service_name
+  ecs_cluster_arn  = var.ecs_cluster_arn
+  ecs_cluster_name = var.ecs_cluster_name
+
+  container_definitions        = var.container_definitions
+  canary_container_definitions = var.canary_container_definitions
+
+  use_auto_scaling = var.use_auto_scaling
+
+  image         = var.image
+  image_version = var.image_version
+
+  # Container definitions configuration
+  cpu    = var.cpu
+  memory = var.memory
+
+  desired_number_of_tasks = var.desired_number_of_tasks
+  max_number_of_tasks     = var.max_number_of_tasks
+  min_number_of_tasks     = var.min_number_of_tasks
+
+  # Canary task configuration
+  canary_version                 = var.canary_version
+  desired_number_of_canary_tasks = var.desired_number_of_canary_tasks
+
+  ecs_node_port_mappings = var.ecs_node_port_mappings
+  vpc_env_var_name       = var.vpc_env_var_name
+
+  # Cloudwatch configuration 
   high_memory_utilization_threshold = var.high_memory_utilization_threshold
-  desired_number_of_canary_tasks    = var.desired_number_of_canary_tasks
   high_memory_utilization_period    = var.high_memory_utilization_period
-  min_number_of_tasks               = var.min_number_of_tasks
-  image                             = var.image
-  image_version                     = var.image_version
   high_cpu_utilization_threshold    = var.high_cpu_utilization_threshold
   high_cpu_utilization_period       = var.high_cpu_utilization_period
-  memory                            = var.memory
-  alarm_sns_topic_arn               = var.alarm_sns_topic_arn
-  kms_master_key_arn                = var.kms_master_key_arn
-  db_primary_endpoint               = var.db_primary_endpoint
-  ecs_instance_security_group_id    = var.ecs_instance_security_group_id
-  ecs_cluster_arn                   = var.ecs_cluster_arn
-  ecs_cluster_name                  = var.ecs_cluster_name
-  container_definitions             = var.container_definitions
+
+  alarm_sns_topic_arn            = var.alarm_sns_topic_arn
+  kms_master_key_arn             = var.kms_master_key_arn
+  db_primary_endpoint            = var.db_primary_endpoint
+  ecs_instance_security_group_id = var.ecs_instance_security_group_id
+
 }
 
