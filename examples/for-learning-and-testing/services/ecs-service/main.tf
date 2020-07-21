7b3b36e50d75f61641cdf7adf4d9240a541cdfe8
Zack Proser <zackproser@gmail.com>
2020-07-21 12:58:28 -0700

    Remove unused variables example

--- examples/for-learning-and-testing/services/ecs-service/main.tf
+++ examples/for-learning-and-testing/services/ecs-service/main.tf
@@ -16,18 +16,13 @@ module "application" {
   ecs_cluster_arn  = var.ecs_cluster_arn
   ecs_cluster_name = var.ecs_cluster_name
 
-  container_definitions        = var.container_definitions
-  canary_container_definitions = var.canary_container_definitions
+  container_definitions = var.container_definitions
 
   use_auto_scaling = true
 
   desired_number_of_tasks = var.desired_number_of_tasks
   max_number_of_tasks     = var.max_number_of_tasks
   min_number_of_tasks     = var.min_number_of_tasks
-
-  # Canary task configuration
-  canary_version                 = var.canary_version
-  desired_number_of_canary_tasks = var.desired_number_of_canary_tasks
 
   ecs_node_port_mappings = var.ecs_node_port_mappings
 
@@ -37,7 +32,6 @@ module "application" {
   high_cpu_utilization_threshold    = var.high_cpu_utilization_threshold
   high_cpu_utilization_period       = var.high_cpu_utilization_period
 
-  kms_master_key_arn   = var.kms_master_key_arn
   alarm_sns_topic_arns = [aws_sns_topic.ecs-alerts.arn]
 }
 
