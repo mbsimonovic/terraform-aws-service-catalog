4d10a9489b59d9b729fc274420628b1e88c999de
Ben Whaley <bwhaley@gmail.com>
2020-07-30 17:44:55 -0700

    Update examples and tests

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -167,3 +167,9 @@ func testSSH(t *testing.T, ip string, sshUsername string, keyPair *aws.Ec2Keypai
 		},
 	)
 }
+
+// PlanWithParallelismE runs terraform plan with the given options including the parallelism flag and returns stdout/stderr.
+// This will fail the test if there is an error in the command.
+func planWithParallelismE(t *testing.T, options *terraform.Options) (string, error) {
+	return terraform.RunTerraformCommandE(t, options, terraform.FormatArgs(options, "plan", "-parallelism=4", "-input=false", "-lock=false")...)
+}
