35b051de7498fd890b8d6c7f72a77379d8c00c8c
Zack Proser <zackproser@gmail.com>
2020-08-05 16:08:57 -0700

    Move aws_region to optional vars

--- examples/for-learning-and-testing/services/ecs-service/variables.tf
+++ examples/for-learning-and-testing/services/ecs-service/variables.tf
@@ -2,11 +2,6 @@
 # REQUIRED PARAMETERS
 # These variables must be passed in by the calling Terraform code.
 # ---------------------------------------------------------------------------------------------------------------------
-variable "aws_region" {
-  description = "The AWS region to deploy into"
-  type        = string
-  default     = "eu-west-1"
-}
 
 variable "ecs_cluster_arn" {
   description = "The ARN of the cluster to which the ecs service should be deployed"
@@ -32,6 +27,11 @@ variable "hosted_zone_id" {
 # OPTIONAL PARAMETERS
 # These values may optionally be overwritten by the calling Terraform code.
 # ---------------------------------------------------------------------------------------------------------------------
+variable "aws_region" {
+  description = "The AWS region to deploy into"
+  type        = string
+  default     = "eu-west-1"
+}
 
 variable "ecs_node_port_mappings" {
   description = "A map of ports used by the Docker containers on an ECS Node. The key should be the container port and the value should be what host port to map it to."
