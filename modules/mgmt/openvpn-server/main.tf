64f0d7b95364cbbda76010ebd9e0b33ebe7207c6
Ben Whaley <bwhaley@gmail.com>
2020-08-19 10:28:29 -0700

    Add comment explaining the situation.

--- modules/mgmt/openvpn-server/main.tf
+++ modules/mgmt/openvpn-server/main.tf
@@ -98,7 +98,12 @@ locals {
   # Merge in all the cloud init scripts the user has passed in
   cloud_init_parts = merge({ default : local.cloud_init }, var.cloud_init_parts)
 
-  kms_key_arn = (var.kms_key_arn != null ?
+  # We use a "double conditional" check here to prevent issues during destroy
+  # operations in which the key from module.kms_cmk has been deleted but 
+  # a subsequent step failed, causing an invalid index error on a subsequent
+  # destroy.
+  kms_key_arn = (
+    var.kms_key_arn != null ?
     var.kms_key_arn :
     length(module.kms_cmk.key_arn) > 0 ?
     module.kms_cmk.key_arn[var.name] :
