19dfdd9dd941737038671aa67bca1645985b2c73
rho song <rhomsang@gmail.com>
2020-08-19 19:18:42 -0500

    Remove security group id output from public cluster example

--- examples/for-learning-and-testing/data-stores/elasticsearch-public/outputs.tf
+++ examples/for-learning-and-testing/data-stores/elasticsearch-public/outputs.tf
@@ -13,11 +13,6 @@ output "cluster_endpoint" {
   value       = module.elasticsearch.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "The ID of the security group created by this module for securing the Elasticsearch cluster."
-  value       = module.elasticsearch.cluster_security_group_id
-}
-
 output "kibana_endpoint" {
   description = "Domain-specific endpoint for Kibana without https scheme."
   value       = module.elasticsearch.kibana_endpoint
