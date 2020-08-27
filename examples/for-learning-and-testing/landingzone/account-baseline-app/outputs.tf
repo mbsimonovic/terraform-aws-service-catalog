b0fc74011ad654af8ac60ca25202eb3da479f4c1
Rob Morgan <robbym@gmail.com>
2020-08-27 19:12:21 +0200

    fix module names

--- examples/for-learning-and-testing/landingzone/account-baseline-app/outputs.tf
+++ examples/for-learning-and-testing/landingzone/account-baseline-app/outputs.tf
@@ -4,7 +4,7 @@
 
 output "config_s3_bucket_names" {
   description = "The names of the S3 bucket used by AWS Config to store configuration items."
-  value       = module.security_baseline.config_s3_bucket_names
+  value       = module.app_baseline.config_s3_bucket_names
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -13,27 +13,27 @@ output "config_s3_bucket_names" {
 
 output "cloudtrail_trail_arn" {
   description = "The ARN of the cloudtrail trail."
-  value       = module.security_baseline.cloudtrail_trail_arn
+  value       = module.app_baseline.cloudtrail_trail_arn
 }
 
 output "cloudtrail_s3_bucket_name" {
   description = "The name of the S3 bucket where cloudtrail logs are delivered."
-  value       = module.security_baseline.cloudtrail_s3_bucket_name
+  value       = module.app_baseline.cloudtrail_s3_bucket_name
 }
 
 output "cloudtrail_kms_key_arn" {
   description = "The ARN of the KMS key used by the S3 bucket to encrypt cloudtrail logs."
-  value       = module.security_baseline.cloudtrail_kms_key_arn
+  value       = module.app_baseline.cloudtrail_kms_key_arn
 }
 
 output "cloudtrail_kms_key_alias_name" {
   description = "The alias of the KMS key used by the S3 bucket to encrypt cloudtrail logs."
-  value       = module.security_baseline.cloudtrail_kms_key_alias_name
+  value       = module.app_baseline.cloudtrail_kms_key_alias_name
 }
 
 output "cloudtrail_cloudwatch_group_name" {
   description = "The name of the cloudwatch log group."
-  value       = module.security_baseline.cloudtrail_cloudwatch_group_name
+  value       = module.app_baseline.cloudtrail_cloudwatch_group_name
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -42,10 +42,10 @@ output "cloudtrail_cloudwatch_group_name" {
 
 output "kms_key_arns" {
   description = "A map from region to ARNs of the KMS CMKs that were created. The value will also be a map mapping the keys from the var.kms_customer_master_keys input variable to the corresponding ARN."
-  value       = module.security_baseline.kms_key_arns
+  value       = module.app_baseline.kms_key_arns
 }
 
 output "kms_key_aliases" {
   description = "A map from region to aliases of the KMS CMKs that were created. The value will also be a map mapping the keys from the var.customer_master_keys input variable to the corresponding alias."
-  value       = module.security_baseline.kms_key_aliases
+  value       = module.app_baseline.kms_key_aliases
 }
