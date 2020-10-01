8d0f9947a58c0cc3571ef5e67c5bda29d797b253
rho song <rhomsang@gmail.com>
2020-10-01 15:44:07 -0700

    Moving error message to log (stderr)

--- modules/tls-scripts/create-tls-cert.sh
+++ modules/tls-scripts/create-tls-cert.sh
@@ -21,7 +21,7 @@
 set -e
 
 if [[ -z $AWS_ACCESS_KEY_ID ]] || [[ -z $AWS_SECRET_ACCESS_KEY ]]; then
-  echo "ERROR: AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set."
+  log "ERROR: AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set."
   exit 1
 fi
 
