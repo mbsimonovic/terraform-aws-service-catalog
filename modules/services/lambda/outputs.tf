3cadcda3c85a2fcc51e9ffe240c50da7cb2e0ccc
Hugo Posca <hugo@gruntwork.io>
2021-04-28 17:16:52 -0700

    Implement PR suggestion - Improving more outputs

--- modules/services/lambda/outputs.tf
+++ modules/services/lambda/outputs.tf
@@ -2,35 +2,43 @@
 # LAMBDA MODULE OUTPUTS
 # ---------------------------------------------------------------------------------------------------------------------
 output "function_name" {
-  value = module.lambda_function.function_name
+  description = "Unique name for Lambda Function"
+  value       = module.lambda_function.function_name
 }
 
 output "function_arn" {
-  value = module.lambda_function.function_arn
+  description = "Amazon Resource Name (ARN) identifying the Lambda Function"
+  value       = module.lambda_function.function_arn
 }
 
 output "iam_role_id" {
-  value = module.lambda_function.iam_role_id
+  description = "Name of the AWS IAM Role created for the Lambda Function"
+  value       = module.lambda_function.iam_role_id
 }
 
 output "iam_role_arn" {
-  value = module.lambda_function.iam_role_arn
+  description = "Amazon Resource Name (ARN) of the AWS IAM Role created for the Lambda Function"
+  value       = module.lambda_function.iam_role_arn
 }
 
 output "security_group_id" {
-  value = module.lambda_function.security_group_id
+  description = "Security Group ID of the Security Group created for the Lambda Function"
+  value       = module.lambda_function.security_group_id
 }
 
 output "invoke_arn" {
-  value = module.lambda_function.invoke_arn
+  description = "Amazon Resource Name (ARN) to be used for invoking the Lambda Function"
+  value       = module.lambda_function.invoke_arn
 }
 
 output "qualified_arn" {
-  value = module.lambda_function.qualified_arn
+  description = "Amazon Resource Name (ARN) identifying your Lambda Function version"
+  value       = module.lambda_function.qualified_arn
 }
 
 output "version" {
-  value = module.lambda_function.version
+  description = "Latest published version of your Lambda Function"
+  value       = module.lambda_function.version
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -81,12 +89,14 @@ output "alarm_arn" {
 }
 
 output "alarm_actions" {
+  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state"
   # alarm.alarm_actions is a set so, without flatten(), we end up with a list
   # of a set (similar to a list of lists). This way we end up with a single list
   value = flatten([for alarm in aws_cloudwatch_metric_alarm.lambda_failure_alarm : alarm.alarm_actions])
 }
 
 output "ok_actions" {
+  description = "The list of actions to execute when this alarm transitions into an OK state from any other state"
   # alarm.ok_actions is a set so, without flatten(), we end up with a list
   # of a set (similar to a list of lists). This way we end up with a single list
   value = flatten([for alarm in aws_cloudwatch_metric_alarm.lambda_failure_alarm : alarm.ok_actions])
