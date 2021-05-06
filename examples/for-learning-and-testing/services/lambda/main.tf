cd782d19a5bcec802c8aa9bdc3658e60c4848772
Hugo Posca <hugo@gruntwork.io>
2021-05-06 16:51:57 -0700

    Implement PR suggestion - Standard comment block
    Added standard comment block that tells customers they should use Git
    URLs instead of local file paths.

--- examples/for-learning-and-testing/services/lambda/main.tf
+++ examples/for-learning-and-testing/services/lambda/main.tf
@@ -7,7 +7,11 @@ provider "aws" {
 }
 
 module "lambda_function" {
-  source      = "../../../../modules/services/lambda"
+  # When using these modules in your own repos, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "git::git@github.com:gruntwork-io/terraform-aws-service-catalog.git//modules/services/lambda?ref=v1.0.8"
+  source = "../../../../modules/services/lambda"
+
   name        = var.name
   description = "Executing some requests to the internet"
 
