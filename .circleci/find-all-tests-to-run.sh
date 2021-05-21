8bc3337dc51e77e3bd6a9810e52b89e23e9e6cb5
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-05-21 10:30:53 +0100

    Extract find all tests to run functionality into script
    This is to work around a bug in yq (https://github.com/mikefarah/yq/issues/828) that prevents us from doing the Terraform upgrades automatically

---
+++ .circleci/find-all-tests-to-run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Add a comment to a PR.
+
+test_regex=""
+if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+  # On master, the last commit is the merge commit which contains all the changes
+  test_regex="$(python ./.circleci/run_tests.py --source-ref 'HEAD^' 2> debug.log)"
+else
+  test_regex="$(python ./.circleci/run_tests.py 2> debug.log)"
+fi
+cat debug.log
+
+# Store the tests to run in a folder which we will write to the CircleCi cache
+mkdir -p /home/circleci/.terraform-aws-service-catalog
+echo "$test_regex" >> /home/circleci/.terraform-aws-service-catalog/test-regex.txt
+
+# Comment the debug log on GitHub if there is a PR number
+CIRCLE_PR_NUMBER="${CIRCLE_PR_NUMBER:-${CIRCLE_PULL_REQUEST##*/}}"
+if [[ -n "$CIRCLE_PR_NUMBER" ]]; then
+  echo -e "Tests run for build [$CIRCLE_BUILD_NUM]($CIRCLE_BUILD_URL)\n\`\`\`\n$(cat debug.log)\n\`\`\`" \
+    | hub api repos/{owner}/{repo}/issues/"$CIRCLE_PR_NUMBER"/comments --field body=@-
+fi
