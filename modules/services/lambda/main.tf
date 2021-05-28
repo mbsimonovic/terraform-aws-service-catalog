f2abac7cf6c03b786dcba9ca49080b236e6af6fd
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-05-28 09:21:39 +0100

    Fix min version in lambda [ci skip]

--- modules/services/lambda/main.tf
+++ modules/services/lambda/main.tf
@@ -4,9 +4,8 @@
 
 terraform {
   # This module is now only being tested with Terraform 0.15.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.15.x code.
-  required_version = ">= 0.12.26"
+  # 0.13 as the minimum version, as this code uses for_each on modules.
+  required_version = ">= 0.13.0"
 
   required_providers {
     aws = {
