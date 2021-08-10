79cb6b40eaa48a3802d3a159ba1a7612f29291c1
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2021-08-10 10:03:37 -0500

    Extract test finder to terraform-aws-ci (#741)
    * Add test mapping
    
    * Convert test finder and mapping hooks to use the one offered by terraform-aws-ci
    
    * Fix pre-commit hook
    
    * Make hook executable
    
    * Only install test finder in the initial containers
    
    * Convert test config to yaml
    
    * Remove for-production ignore
    
    * Use released version of monorepo-helpers
    
    * Sync ignore config and test mapping list
    
    * Add comments about monorepo config files; Convert few ignore files to test mappings
    
    * ignore_config is for validate, not find

--- .circleci/find-all-tests-to-run.sh
+++ .circleci/find-all-tests-to-run.sh
@@ -4,9 +4,9 @@
 test_regex=""
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
   # On master, the last commit is the merge commit which contains all the changes
-  test_regex="$(python ./.circleci/run_tests.py --source-ref 'HEAD^' 2> debug.log)"
+  test_regex="$(find-tf-monorepo-tests --source-ref 'HEAD^' 2> debug.log)"
 else
-  test_regex="$(python ./.circleci/run_tests.py 2> debug.log)"
+  test_regex="$(find-tf-monorepo-tests 2> debug.log)"
 fi
 cat debug.log
 
--- hooks/run_validate_test_mappings.sh
+++ hooks/run_validate_test_mappings.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-# Get the repo root and add ".circleci" to the python path
-readonly repo_root="$(git rev-parse --show-toplevel | tr -d '\n')"
-PYTHONPATH="$repo_root/.circleci" python "$repo_root/hooks/validate_test_mappings.py"
+# Ideally this script is not necessary, but pre-commit has a hard time finding the actual binary to run, so we use a
+# script as an intermediary.
+validate-monorepo-test-mappings
