70b290415e2873e6cdbbbc376bbf23fd63a3623e
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2021-11-18 09:42:33 -0600

    Expose name of ECS cluster (#1051)

--- modules/services/ecs-fargate-cluster/outputs.tf
+++ modules/services/ecs-fargate-cluster/outputs.tf
@@ -2,3 +2,8 @@ output "arn" {
   description = "ARN of the ECS cluster that was created."
   value       = aws_ecs_cluster.fargate_only.arn
 }
+
+output "name" {
+  description = "The name of the ECS cluster."
+  value       = aws_ecs_cluster.fargate_only.name
+}
