1790da41c93d88bcdb911aab45d376b7e675a07c
gruntwork-patcher <94529884+gruntwork-patcher@users.noreply.github.com>
2022-03-07 18:29:22 +0000

    Update dependency helm/helm to v3.8.0 (#1271)

--- modules/mgmt/jenkins/install.sh
+++ modules/mgmt/jenkins/install.sh
@@ -26,7 +26,7 @@ readonly DEFAULT_TERRAFORM_VERSION="1.0.10"
 # terraform-aws-eks, both of which lag behind open source K8S
 readonly DEFAULT_KUBECTL_VERSION="v1.19.1"
 # patcher auto-update-github-releases: helm/helm
-readonly DEFAULT_HELM_VERSION="v3.7.2"
+readonly DEFAULT_HELM_VERSION="v3.8.0"
 # patcher auto-update-github-releases: hashicorp/packer
 readonly DEFAULT_PACKER_VERSION="1.7.8"
 # patcher auto-update-docker-ubuntu
